e7f44d740a65337abdd3221256ce2d86a4c500f1
vietanhlee <levietanhtrump@gmail.com>
2025-10-18 14:51:18 +0700

    thêm utils cho chat bot (trim message)

---
+++ Backend/app/utils/chatbot_utils.py
@@ -0,0 +1,16 @@
+from langchain_core.messages.utils import (
+    trim_messages,
+    count_tokens_approximately
+)
+
+# This function will be called every time before the node that calls LLM
+def pre_model_hook(state):
+    trimmed_messages = trim_messages(
+        state["messages"],
+        strategy="last",
+        token_counter=count_tokens_approximately,
+        max_tokens=384,
+        start_on="human",
+        end_on=("human", "tool"),
+    )
+    return {"llm_input_messages": trimmed_messages}
