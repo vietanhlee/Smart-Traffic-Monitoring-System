ae45e6140c2a67fd50374d83ae55777181a53eee
vietanhlee <levietanhtrump@gmail.com>
2025-10-20 15:56:40 +0700

    sửa lại thời gian tồn tại của token

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -9,7 +9,7 @@ class SettingServer:
     DATABASE_URL = os.getenv("DATABASE_URL")
     JWT_SECRET = os.getenv("JWT_SECRET_KEY")
     JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    ACCESS_TOKEN_EXPIRE_DAYS = int(os.getenv("ACCESS_TOKEN_EXPIRE_DAYS"))
 
 
 class SettingMetricTransport:
